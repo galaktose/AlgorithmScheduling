22c69f7030eedc97f223d425aba36c9d3dff1c33
Hannan <1211104293@student.mmu.edu.my>
2024-01-08 21:10:28 +0800

    progress on non pre sjf

--- NonPreSJF.java
+++ NonPreSJF.java
@@ -23,8 +23,38 @@ public class NonPreSJF extends Processes{
         for (int i = 0; i < processes; i++) {
             System.out.print(burstTime[i] + " ");
         }
+        System.out.println(" ");
 
-        //SJF Gantt chart generation
-        
+        //Non pre emptive SJF Gantt chart generation
+
+        //check total time
+        int scheduleTime = 0;
+        for (int i = 0; i < processes; i++) {
+            scheduleTime =scheduleTime + burstTime[i];
+        }
+
+        System.out.println("Shortest Job First(Non Preemptive) Gantt Chart \n");
+        //check arrival time
+       
+        System.out.print("|");
+        for (int i = 0; i < scheduleTime - 1; i++) {
+            System.out.print("----|");
+        }
+        System.out.print("\n");
+        for (int i = 0; i < scheduleTime; i++) {
+            System.out.print((i+1) + "    ");
+        }
+
+        //display arrival time
+        for (int index = 0; index < processes; index++) {
+            
+        }
+
     }
 }
+ // int currentArrival = arrivalTime[0];
+        // for (int i = 1; i < processes; i++) {
+        //     if (arrivalTime[i] < currentArrival) {
+        //         currentArrival = arrivalTime[i];
+        //     }
+        // }
