5264885e20b005d67f86f6d70a37513d7b2ca288
Hannan <1211104293@student.mmu.edu.my>
2024-01-18 01:46:23 +0800

    processes that come in now display beneath gantt chart

--- NonPreSJF.java
+++ NonPreSJF.java
@@ -5,12 +5,14 @@ public class NonPreSJF extends Processes{
         processes = scanner.nextInt();
         int arrivalTime[] = new int[processes];
         int burstTime[] = new int[processes];
+        int processNumber[] = new int[processes];
         System.out.println("Input process information: ");
         while (count < processes) {
             System.out.println("Process " + (1 + count) + " Arrival Time : ");
             arrivalTime[count] = scanner.nextInt();
             System.out.println("Process " + (1 + count) + " Burst Time : ");
             burstTime[count] = scanner.nextInt();
+            processNumber[count] = count + 1;
             count++;
         }
 
@@ -24,6 +26,43 @@ public class NonPreSJF extends Processes{
             System.out.print(burstTime[i] + " ");
         }
         System.out.println(" ");
+        System.out.println("Process Numbers: ");
+        for (int i = 0; i < processes; i++) {
+            System.out.print(processNumber[i] + " ");
+        }
+        System.out.println(" ");
+
+        //sort arrival times according to their length, with burst time and process number following suit
+        int tempInt = 0;
+        for (int index = 0; index < processes - 1; index++) {
+            if (arrivalTime[index] > arrivalTime[index+1]) {
+                tempInt = arrivalTime[index];
+                arrivalTime[index] = arrivalTime[index+1];
+                arrivalTime[index+1] = tempInt;
+
+                tempInt = burstTime[index];
+                burstTime[index] = burstTime[index+1];
+                burstTime[index+1] = tempInt;
+
+                tempInt = processNumber[index];
+                processNumber[index] = processNumber[index+1];
+                processNumber[index+1] = tempInt;
+            }
+        }
+
+        //for testing purposes only
+        // for (int i = 0; i < processes; i++) {
+        //     System.out.print(arrivalTime[i] + " ");
+        // }
+        // System.out.println();
+        // for (int i = 0; i < processes; i++) {
+        //     System.out.print(burstTime[i] + " ");
+        // }
+        // System.out.println();
+        // for (int i = 0; i < processes; i++) {
+        //     System.out.print(processNumber[i] + " ");
+        // }
+        // System.out.println();
 
         //Non pre emptive SJF Gantt chart generation
 
@@ -33,28 +72,65 @@ public class NonPreSJF extends Processes{
             scheduleTime =scheduleTime + burstTime[i];
         }
 
+        //create process arrival time line
+        String processArrival = "";
+        count = 0;
+        for (int currentTime = 0; currentTime < scheduleTime; currentTime++) {
+            for (int index = 0; index < processes; index++) {
+                if (arrivalTime[index] == currentTime) {
+                    processArrival = processArrival + "P" + processNumber[index] + "("+burstTime[index]+")" + "";
+                    break;
+                }
+                else
+                {
+                    processArrival = processArrival + "   ";
+                }
+            }
+        }
+
         System.out.println("Shortest Job First(Non Preemptive) Gantt Chart \n");
         //check arrival time
        
         System.out.print("|");
         for (int i = 0; i < scheduleTime - 1; i++) {
-            System.out.print("----|");
+            System.out.print("------|");
         }
         System.out.print("\n");
         for (int i = 0; i < scheduleTime; i++) {
-            System.out.print((i+1) + "    ");
-        }
-
-        //display arrival time
-        for (int index = 0; index < processes; index++) {
+            if (i < 9) {
+                System.out.print((i) + "      ");
+            } 
+            else {
+                System.out.print((i) + "   ");
+            }
             
         }
+        System.out.println("\n");
+
+        
+        System.out.println(processArrival);
+
+        int remainingBurstTime[] = new int[processes];
+        for (int i = 0; i < processes; i++) {
+            remainingBurstTime[i] = burstTime[i];
+        }
+
+        
 
     }
+
+    private int findShortestJobIndex(int[] remainingBurstTime, int currentTime, int[] arrivalTime) {
+        int shortestJobIndex = -1;
+        int shortestBurstTime = Integer.MAX_VALUE;
+
+        for (int i = 0; i < processes; i++) {
+            if //(remainingBurstTime[i] > 0 && arrivalTime[i] <= currentTime && remainingBurstTime[i] < shortestBurstTime) {
+                (remainingBurstTime[i] > 0 && remainingBurstTime[i] < shortestBurstTime && arrivalTime[i] == currentTime){
+                shortestBurstTime = remainingBurstTime[i];
+                shortestJobIndex = i;
+            }
+        }
+
+        return shortestJobIndex;
+    }
 }
- // int currentArrival = arrivalTime[0];
-        // for (int i = 1; i < processes; i++) {
-        //     if (arrivalTime[i] < currentArrival) {
-        //         currentArrival = arrivalTime[i];
-        //     }
-        // }
